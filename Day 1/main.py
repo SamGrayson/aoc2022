a6668c099fee53a2d0935ac527019b49649fa59c
Sam <samuel.c.grayson17@gmail.com>
2022-12-01 10:46:09 -0500

    Day 1 solution in python

---
+++ Day 1/main.py
@@ -0,0 +1,22 @@
+import bisect
+
+def count_calories(top_count=3):
+    count_list = []
+    current_count = 0
+
+    with open('Day 1/input.txt') as calorie_input:
+        for line in calorie_input:
+            if line == '\n':
+                bisect.insort(count_list, current_count) 
+                current_count = 0
+            else:
+                current_count += int(line.split('\n')[0])
+        else:
+            bisect.insort(count_list, current_count) 
+            
+    list_end = len(count_list)
+    
+    return count_list[list_end-top_count : list_end]
+
+max = count_calories()
+print(sum(max))
