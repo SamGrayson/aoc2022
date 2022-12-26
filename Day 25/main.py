a81d7172bcc6bab2d27627cba81253a916b6d8f4
Sam <samuel.c.grayson17@gmail.com>
2022-12-26 09:45:52 -0500

    Day 25 - solved

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,10 +1,42 @@
+num_key = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+decimal_key = {0: "0", 1: "1", 2: "2", 3: "=", 4: "-"}
+
+
+def convert(num):
+    read = num[::-1]
+    i = 0
+    result = 0
+    for n in read:
+        if i == 0:
+            power = 1
+        else:
+            power = 5**i
+        result += num_key[n] * power
+        i += 1
+
+    return result
+
+
+def convert_to_snafu(num):
+    snafu = []
+    while num > 0:
+        num, mod = divmod(num, 5)
+        snafu.insert(0, decimal_key[mod])
+        if mod > 2:
+            num += 1
+    return "".join(snafu)
+
+
 def part_1():
 
-    with open("Day <ins>/input_sample.txt") as data:
+    values = []
+
+    with open("Day 25/input.txt") as data:
         temp = data.read().splitlines()
         for line in temp:
+            values.append(convert(line.strip()))
 
-            pass
+    return convert_to_snafu(sum(values))
 
 
 res = part_1()
